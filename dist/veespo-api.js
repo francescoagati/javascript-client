8863d5ef42400cee177a36ef18571699dfb80235
francescoagati <francescoagati1975@gmail.com>
2014-02-24 09:04:50 +0100

    bower file and jquery

--- dist/veespo-api.js
+++ dist/veespo-api.js
@@ -1,5 +1,18 @@
 (function() {
-  var veespo;
+  var Api, veespo;
+
+  Api = (function() {
+    function Api(params) {
+      if (params == null) {
+        params = {};
+      }
+      this.token = params.token;
+      this.api_key = params.api_key;
+    }
+
+    return Api;
+
+  })();
 
   if (typeof veespo === "undefined" || veespo === null) {
     veespo = {};
--- src/veespo-api.coffee
+++ src/veespo-api.coffee
@@ -1,1 +1,11 @@
+class Api
+  constructor: (params={}) ->
+    @token   = params.token
+    @api_path = params.api_path
+  get: (path,params={}) ->
+    params.callback="?"
+    jQuery.get("#{api_path}/#{path}",params)
+
+
 veespo = {} unless veespo?
+
